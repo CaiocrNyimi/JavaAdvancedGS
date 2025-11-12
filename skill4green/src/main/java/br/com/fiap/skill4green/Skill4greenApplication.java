ec055629089f8adaa6d385035d3a45f4e1773348
CaiocrNyimi <caiocrnyimi@gmail.com>
2025-11-12 11:02:13 -0300

    first commit

---
+++ skill4green/src/main/java/br/com/fiap/skill4green/Skill4greenApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.skill4green;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skill4greenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Skill4greenApplication.class, args);
+	}
+
+}
